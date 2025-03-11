9eb28ecabd31ce80bf4180d78b72bb145fa4a70b
rtexada1@pvamu.edu <rtexada1@pvamu.edu>
2025-03-11 11:56:08 -0500

    Added user input feature

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	return 0;
 }
