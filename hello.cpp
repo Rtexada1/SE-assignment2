324710f9963fe78861e14f75e9c6866e9dc860de
rtexada1@pvamu.edu <rtexada1@pvamu.edu>
2025-03-11 12:35:19 -0500

    Updated hello.cpp in feature-2 branch

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	string name1;
+	string name2;
 	cout << "Enter your name: ";
-	cin >> name1;
-	cout << "Hello, " << name1 << "!" << endl;
+	cin >> name2;
+	cout << "Hello, " << name2 << "!" << endl;
 	return 0;
 }
