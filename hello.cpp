6645c769f53260ad8e18dd194cb85dea323cc77b
rtexada1@pvamu.edu <rtexada1@pvamu.edu>
2025-03-11 13:43:15 -0500

    Changed welcome message in feature-2

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,5 @@ int main() {
 	string name1;
 	cout << "Enter your name: ";
 	cin >> name2;
-	cout << "Hello, " << name2 << "!" << endl;
-	return 0;
+	cout << "Hello, " << name2 << "! GitHub is great for teamwork" << endl;
 }
